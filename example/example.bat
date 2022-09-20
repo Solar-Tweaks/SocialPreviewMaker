6ff6b1241a6b1ae02890bab74528daceb30848a6
ZeoNight <zeonightyt@gmail.com>
2022-09-20 19:42:05 +0300

    refactor: :recycle: Use github rest API for project information (#1)
    Use the github rest API for project information, this makes it easier for the user to create a preview fast by just supplying the full name of the repository.

--- example/example.bat
+++ example/example.bat
@@ -1,2 +1,2 @@
 @echo off
-npm start -- --title=Solar-Tweaks/Solar-Tweaks "--description=Solar Tweaks is a tool for tweaking and\ncustomizing Lunar Client. Give yourself\nan upgrade in your gaming experience." --tags=electron,minecraft,lunarclient,solartweaks
+npm start -- --repo=Solar-Tweaks/SocialPreviewMaker
